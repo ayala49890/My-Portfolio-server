b9ff3709f07ab5ae6d5ec0f280352e3881350c51
ayala49890 <ayala49890@gmail.com>
2025-07-24 16:39:39 +0300

    to railway

--- Portfolio.API/obj/Debug/net8.0/Portfolio.API.AssemblyInfo.cs
+++ Portfolio.API/obj/Debug/net8.0/Portfolio.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e95aadbd672bfd84c2259018f9e40cbe240a748")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7516a925774d853091be01481992ee899eb4195b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
